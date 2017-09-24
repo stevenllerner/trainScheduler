61b413cc2c7a43b05c6c5ef54b5780d62c34553f
Steven L Lerner <stevenllerner@gmail.com>
2017-09-24 10:07:24 -0500

    Final version

--- train.js
+++ train.js
@@ -9,14 +9,7 @@ var config = {
 };
 firebase.initializeApp(config);
 
-//$("#currentTime").html(Date());
-
 var trainDb = firebase.database();
-var frequency;
-//trainDb.ref().on('value',function(snapshot){
-//	console.log(snapshot);
-//});
-
 
 //When submit button is pressed, data is added to firebase
 $(".btn").on("click",(function(){
@@ -33,41 +26,43 @@ $(".btn").on("click",(function(){
 		"firstTime": firstTime,
 		"frequency": freq,
 	});
+//Clear submit area after data has been stored
+	$("#trainName").val("");
+	$("#destination").val("");
+	$("#time").val("");
+	$("#freq").val("");
 }));
 
 //When page is loaded, add everything in database to the table on top of the page. When new data is added by user, add new data to table.
 trainDb.ref('/trains').on("child_added", function(trainSnap, indx){
-//	console.log(trainSnap.val());
-//	console.log(trainSnap.val().trainName);
-	$(".tdTrainName").append(trainSnap.val().trainName);
-	$(".tdDestination").append(trainSnap.val().destination);
-	$(".tdFrequency").append(trainSnap.val().frequency);
+	$(".tdTrainName").append(trainSnap.val().trainName, "<br>");
+	$(".tdDestination").append(trainSnap.val().destination, "<br>");
+	$(".tdFrequency").append(trainSnap.val().frequency, "<br>");
+
 // initTime is provided in military time HH:mm	
 	var initTime = trainSnap.val().firstTime;
-	console.log(initTime);
 	var hours = initTime[0].toString()+initTime[1].toString();
 	var minutes = initTime[3].toString()+initTime[4].toString();
-	console.log(hours);
-	console.log(minutes);
+
 //xStartOfDay is the start of today in X format
 	var xStartOfDay = parseFloat(moment().startOf("day").format("X"));
-	console.log(xStartOfDay);
+
 //xCurrentTime is the current time in X format
 	var xCurrentTime = parseFloat(moment().format("X"));
-	console.log(xCurrentTime);
+
 //xInitTrain is the first train time in X format
 	var xInitTrain = parseFloat(moment().startOf("day").add(hours,"hours").add(minutes,"minutes").format("X"));
-	console.log(xInitTrain);
 //Need to find out how many minutes to next train
 	var frequency = trainSnap.val().frequency;
 	freq = parseFloat(frequency);
 	var minNextTrain = freq - (( (xCurrentTime-xInitTrain)/60 )%freq);
-	console.log(minNextTrain);
 	minNextTrain = Math.round(minNextTrain);
-//	minNextTrain = parseInt(minNextTrain);
-	console.log(minNextTrain);
-	var nextArrival = moment().add(minNextTrain,"minutes").format("HH:mm A");
-	console.log(nextArrival);	
+	var nextArrival = moment().add(minNextTrain,"minutes").format("hh:mm A");
+
+//Complete table entry by adding Next Arrival and Minutes Away
+	$(".tdNextArrival").append(nextArrival, "<br>");
+	$(".tdMinutesAway").append(minNextTrain, "<br>");
+
 
 
 
@@ -92,7 +87,7 @@ trainDb.ref('/trains').on("child_added", function(trainSnap, indx){
 //	console.log(currentTime);
 //	var difference = initTrainTime.diff(currentTime);
 //	console.log(difference);
-})
+});
 
 //Set up click event for submit button
 //$(".btn").click(function(){
